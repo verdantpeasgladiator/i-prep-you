d518ce4fbda29b96620def12098b0f021507a167
Anne <anneguo3@gmail.com>
2019-10-13 02:25:52 -0700

    Temporary commit to update all other code from master

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import './App.css';
-import ScoreCard from './Components/ScoreCard.js';
-import MyWebcam from './Components/Webcam';
-import Feedback from './Components/Feedback';
-import Interview from './Components/Interview';
+import React from "react";
+import "./App.css";
+import ScoreCard from "./Components/ScoreCard.js";
+import MyWebcam from "./Components/Webcam";
+import Feedback from "./Components/Feedback";
+import Interview from "./Components/Interview";
+import Input from "./Components/FileUpload";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Input />
         <ScoreCard />
         <Feedback />
         <Interview>
