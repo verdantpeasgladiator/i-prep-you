76626d5040bf2f693d1c43a8dea8c05a5c6e8278
Ian <ianmmah@gmail.com>
2019-10-13 01:48:34 -0700

    speech to text

--- src/Components/Interview.js
+++ src/Components/Interview.js
@@ -1,28 +1,57 @@
 import React from "react";
 import MyWebcam from './webcam';
+const SpeechSDK = require("microsoft-cognitiveservices-speech-sdk");
+const subscriptionKey = "cfd23720fb3c4a5d9c28649d946259a1";
+const serviceRegion = "westus"; // e.g., "westus"
 
 export default class Interview extends React.Component {
     constructor() {
       super()
       this.state = {
-        isRecording: false
-      } 
+        isRecording: false,
+        textOutput: ''
+      }
     }
 
     switchRecording () {
       this.setState({
         isRecording: !this.state.isRecording
-      }) 
+      })
     }
-  
+
+    speechSDK () {
+      // if we got an authorization token, use the token. Otherwise use the provided subscription key
+       var speechConfig = SpeechSDK.SpeechConfig.fromSubscription(subscriptionKey, serviceRegion);
+
+       speechConfig.speechRecognitionLanguage = "en-US";
+       var audioConfig  = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
+       var recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
+       recognizer.recognizeOnceAsync(
+         (result) => {
+           console.log(result);
+           this.setState({
+             textOutput: result.text
+           })
+           recognizer.close();
+           recognizer = undefined;
+         },
+         (err) => {
+           console.log(err);
+           recognizer.close();
+           recognizer = undefined;
+         });
+    }
+
     render() {
       if (this.state.isRecording){
         return (
           <div>
              <button onClick={this.switchRecording.bind(this)}>Stop Interview</button>
-             <MyWebcam></MyWebcam>
-          </div>          
-          ); 
+             <MyWebcam />
+             <button onClick={this.speechSDK.bind(this)}>Start Speech</button>
+             <p>{ this.state.textOutput }</p>
+          </div>
+          );
       } else {
         return (
           <div>
